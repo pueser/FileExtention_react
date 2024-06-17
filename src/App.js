7caddeb7fc3ae1b520caf32dca15b354510f49a3
pueser <hwanghyejin932@gmail.com>
2024-06-17 15:48:14 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+
+function App() {    
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    let formData = new FormData();
+    formData.append("userfile", document.frm.uploadFile.files[0]);
+    axios({
+      method:'post',
+      //url:'http://localhost:8090/fileUpload/extention',
+      url:'http://localhost:8090/fileDownload/pdf',
+      data: formData,
+      responseType: 'arraybuffer',
+      
+    })
+    .then((result)=>{console.log('요청성공')
+    console.log(result)
+   download(result.data);
+  
+   
+   
+   
+
+    })
+      .catch((error)=>{console.log('요청실패')
+      console.log(error)
+    })
+
+  }
+  
+
+async function download(arrayBuffer){
+
+  console.log("arrayBuffer = ", arrayBuffer);
+
+ 
+
+  const blob = new Blob([arrayBuffer], { type: 'application/octet-stream' });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.href = url;
+  link.setAttribute('download', 'downloaded_file.pdf');
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
+
+};
+
   return (
-    <div className="App">
-      안녕하세요
-    </div>
+    <form name="frm" onSubmit={onSubmit} encType="multipart/form-data">
+  
+    
+    <input type="file" name="uploadFile" accept='*' />
+    <input type="submit" value="upload" />
+    
+  </form>
   );
 }
 
